a736870f5c3990177c3453e605755ac30a71b92d
nyankosama <hlr10@software.nju.edu.cn>
2014-11-03 20:05:04 +0800

    cycle twocolor solutions

---
+++ src/main/java/com/nyankosama/algorithm/datastructure/graph/Cycle.java
@@ -0,0 +1,32 @@
+package com.nyankosama.algorithm.datastructure.graph;
+
+/**
+ * Created by i@nyankosama.com on 2014/11/3.
+ */
+public class Cycle {
+    //NOTE 判断一个图中是否存在循环
+
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph G) {
+        marked = new boolean[G.V()];
+        for (int s = 0; s < G.V(); s++) {
+            if (!marked[s]) dfs(G, s, s);
+        }
+    }
+
+    private void dfs(Graph G, int v, int u) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w])
+                dfs(G, w, v);
+            else if (w != u)
+                hasCycle = true;
+        }
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+}
