631f10b99b55b713258d0e3f3b1f1ebeb7b2239f
nyankosama <hlr10@software.nju.edu.cn>
2014-12-04 20:28:30 +0800

    add max min delete methods to BST

--- src/main/java/com/nyankosama/algorithm/search/BST.java
+++ src/main/java/com/nyankosama/algorithm/search/BST.java
@@ -29,6 +29,67 @@ public class BST<Key extends  Comparable<Key>, Value> {
         else return x.N;
     }
 
+    public Node max() {
+        return max(root);
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) return x;
+        else return max(x.right);
+    }
+
+    public Node min() {
+        return min(root);
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.right) + size(x.left) + 1;
+        return x;
+    }
+
+    public void deleteMax() {
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.N = size(x.right) + size(x.left) + 1;
+        return x;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp > 0) x.right = delete(x.right, key);
+        if (cmp < 0) x.left = delete(x.left, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
     public Value get(Key key) {
         return get(root, key);
     }
